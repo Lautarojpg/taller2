72b4588b944809ba7725451f8af2004f29612456
Lautaro <lautarofranciscolezcano@gmail.com>
2025-08-13 09:49:11 -0300

    practico 1

---
+++ Practico1/Practico1/Formulario1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Formulario1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BGuardar = New System.Windows.Forms.Button()
+        Me.BEliminar = New System.Windows.Forms.Button()
+        Me.LApellido = New System.Windows.Forms.Label()
+        Me.LNombre = New System.Windows.Forms.Label()
+        Me.TxtApellido = New System.Windows.Forms.TextBox()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.TxtNombre = New System.Windows.Forms.TextBox()
+        Me.BSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BGuardar
+        '
+        Me.BGuardar.Location = New System.Drawing.Point(37, 254)
+        Me.BGuardar.Name = "BGuardar"
+        Me.BGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.BGuardar.TabIndex = 0
+        Me.BGuardar.Text = "Guardar"
+        Me.BGuardar.UseVisualStyleBackColor = True
+        '
+        'BEliminar
+        '
+        Me.BEliminar.Location = New System.Drawing.Point(154, 254)
+        Me.BEliminar.Name = "BEliminar"
+        Me.BEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.BEliminar.TabIndex = 1
+        Me.BEliminar.Text = "Eliminar"
+        Me.BEliminar.UseVisualStyleBackColor = True
+        '
+        'LApellido
+        '
+        Me.LApellido.AutoSize = True
+        Me.LApellido.Location = New System.Drawing.Point(52, 130)
+        Me.LApellido.Name = "LApellido"
+        Me.LApellido.Size = New System.Drawing.Size(44, 13)
+        Me.LApellido.TabIndex = 2
+        Me.LApellido.Text = "Apellido"
+        '
+        'LNombre
+        '
+        Me.LNombre.AutoSize = True
+        Me.LNombre.Location = New System.Drawing.Point(52, 187)
+        Me.LNombre.Name = "LNombre"
+        Me.LNombre.Size = New System.Drawing.Size(44, 13)
+        Me.LNombre.TabIndex = 3
+        Me.LNombre.Text = "Nombre"
+        '
+        'TxtApellido
+        '
+        Me.TxtApellido.Location = New System.Drawing.Point(115, 127)
+        Me.TxtApellido.Name = "TxtApellido"
+        Me.TxtApellido.Size = New System.Drawing.Size(100, 20)
+        Me.TxtApellido.TabIndex = 4
+        '
+        'TextBox
+        '
+        Me.TextBox.Location = New System.Drawing.Point(242, 127)
+        Me.TextBox.Multiline = True
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(203, 150)
+        Me.TextBox.TabIndex = 5
+        '
+        'TxtNombre
+        '
+        Me.TxtNombre.Location = New System.Drawing.Point(115, 184)
+        Me.TxtNombre.Name = "TxtNombre"
+        Me.TxtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.TxtNombre.TabIndex = 6
+        '
+        'BSalir
+        '
+        Me.BSalir.Location = New System.Drawing.Point(369, 326)
+        Me.BSalir.Name = "BSalir"
+        Me.BSalir.Size = New System.Drawing.Size(75, 23)
+        Me.BSalir.TabIndex = 7
+        Me.BSalir.Text = "Salir"
+        Me.BSalir.UseVisualStyleBackColor = True
+        Me.BSalir.Visible = False
+        '
+        'Formulario1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 361)
+        Me.Controls.Add(Me.BSalir)
+        Me.Controls.Add(Me.TxtNombre)
+        Me.Controls.Add(Me.TextBox)
+        Me.Controls.Add(Me.TxtApellido)
+        Me.Controls.Add(Me.LNombre)
+        Me.Controls.Add(Me.LApellido)
+        Me.Controls.Add(Me.BEliminar)
+        Me.Controls.Add(Me.BGuardar)
+        Me.KeyPreview = True
+        Me.Name = "Formulario1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mi primer Forms"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BGuardar As Button
+    Friend WithEvents BEliminar As Button
+    Friend WithEvents LApellido As Label
+    Friend WithEvents LNombre As Label
+    Friend WithEvents TxtApellido As TextBox
+    Friend WithEvents TextBox As TextBox
+    Friend WithEvents TxtNombre As TextBox
+    Friend WithEvents BSalir As Button
+End Class
---
+++ Practico1/Practico1/Formulario1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Formulario1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        Dim nombreCompleto As String = txtNombre.Text & " " & txtApellido.Text
+        TextBox.Text = nombreCompleto
+    End Sub
+
+    Private Sub Formulario1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles LApellido.Click
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TxtNombre.TextChanged
+
+    End Sub
+
+    Private Sub BEliminar_Click(sender As Object, e As EventArgs) Handles BEliminar.Click
+        TextBox.Clear()
+        TxtNombre.Clear()
+        TxtApellido.Clear()
+    End Sub
+
+    Private Sub Formulario1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.Control AndAlso e.KeyCode = Keys.S Then
+            BSalir.Visible = True
+        End If
+    End Sub
+
+    Private Sub BSalir_Click(sender As Object, e As EventArgs) Handles BSalir.Click
+        End
+    End Sub
+End Class
